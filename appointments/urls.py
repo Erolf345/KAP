a2b908ccd8da19c3483b4dd46fb084a4e945fd88
Erik Riedel <E.P.Riedel@gmail.com>
2019-10-22 10:30:51 +0200

    Improved appointments app integration

--- appointments/urls.py
+++ appointments/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('', views.appointment_list),
+    path('', views.appointment_list,name='appointments_list'),
     path('create/', views.appointment_create),
     path('detail/<int:id>/', views.appointment_detail, name='detail'),
     path('list/', views.appointment_list),
